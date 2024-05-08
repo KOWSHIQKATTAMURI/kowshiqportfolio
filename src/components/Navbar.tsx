a4ba9768cc9999e49c7864cb0ab6886fde5e7fe0
naveengitboi <naveenjangiti9@gmail.com>
2024-05-08 23:14:32 +0530

    responsive till tablet mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,11 @@ const Navbar:React.FC = ()=> {
             <div className="navbar">
             <h1 className="logo mediumHeading secFont ">kowshiq</h1>
             <ul className="navLinks priFont">
-                <li className='activeLink'>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
+                <li className='activeLink'><a href="#about">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#about">Skills</a></li>
+                <li><a href="#about">Portfolio</a></li>
+                <li><a href="#about">Contact</a></li>
                 <li><a href="https://www.amazon.in/Wizard-Problems-Mathematics-Kowshiq-Kattamuri/dp/9390447712" target='_blank'>My Book</a></li>
             </ul>
         </div>
