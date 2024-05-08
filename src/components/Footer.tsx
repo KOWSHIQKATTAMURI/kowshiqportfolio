9a5f1e5fe95d838ffeceef3ec49fc5ab4d0e23d5
naveengitboi <naveenjangiti9@gmail.com>
2024-05-08 15:49:24 +0530

    added footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,8 @@
 import '../componentsStyles/Footer.css'
-import { FaGithub } from "react-icons/fa";
+import { FaGithub,FaYoutube } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { LiaCopyrightSolid } from "react-icons/lia";
+
 
 const Footer:React.FC = () => {
     return(
@@ -7,10 +10,18 @@ const Footer:React.FC = () => {
             <div className="footerContent">
                 <p className='largeHeading'>Kattamuri Kowshiq</p>
                 <div className="footerSocialMedia">
-                        <a href="" className='darkBgButton'>
+                        <a href="https://github.com/KOWSHIQKATTAMURI"  target='_blank' rel="noreferrer"  className='darkBgButton'>
                             <FaGithub className="svgIcon" />
                         </a>
+                        <a href="" rel="noreferrer" target='_blank' className='darkBgButton'>
+                            <FaYoutube className="svgIcon" />
+                        </a>
+                        <a href="" rel="noreferrer" target='_blank'  className='darkBgButton'>
+                            <IoLogoWhatsapp className="svgIcon" />
+                        </a>
                 </div>
+
+                <p className='tinyText ccText'> <LiaCopyrightSolid/> Kowshiq. All Rights Reserved</p>
             </div>
         </div>
     )
