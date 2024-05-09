7e23de518e97a50366440cb08348248f13965c1b
naveengitboi <naveenjangiti9@gmail.com>
2024-05-09 16:50:40 +0530

    added hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import '../componentsStyles/Navbar.css'
 
 const Navbar:React.FC = ()=> {
+    const [openHam, setOpenHam] = useState(false)
     return(
         <div className="header">
             <div className="navbar">
             <h1 className="logo mediumHeading secFont ">kowshiq</h1>
-            <ul className="navLinks priFont">
+            <div className={openHam ? "hamburgerMenu" : "hamburgerMenu activeHamburgerLines"} onClick={() => setOpenHam(prev => !prev)} >
+                <div className='hamLine firstHamLine'></div>
+                <div className='hamLine secondHamLine'></div>
+                <div className='hamLine thirdHamLine'></div>
+            </div>
+            <ul className={openHam ? "navLinks priFont mobileMode" : "navLinks priFont mobileMode openNavLinks"}>
                 <li className='activeLink'><a href="#about">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#about">Skills</a></li>
