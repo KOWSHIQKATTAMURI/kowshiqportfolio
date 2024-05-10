a74d2b6e1040ad1e6c6ec4bf30e09c848392759d
naveengitboi <naveenjangiti9@gmail.com>
2024-05-10 21:31:29 +0530

    changes minor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar:React.FC = ()=> {
                 <li className='activeLink'><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
+                <li><a href="#project">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
                 <li><a href="https://www.amazon.in/Wizard-Problems-Mathematics-Kowshiq-Kattamuri/dp/9390447712" target='_blank'>My Book</a></li>
             </ul>
